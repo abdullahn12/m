94fca09808d1026d6b6f666cc7b3d746b03d9b02
abdullahn12 <mh.abdullah.13386s@gmail.com>
2025-03-21 14:59:09 +0000

    Add project files

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port: 5173,
-    strictPort: true
-  },
-  base: './'
+  base: '/',
+  build: {
+    outDir: 'dist',
+    sourcemap: false
+  }
 });
